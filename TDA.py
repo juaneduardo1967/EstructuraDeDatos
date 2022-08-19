fa7408c82602a3af0e33d0cd91044f9f98e38fb6
Juan <juaneduardo.lacava@estudiantes.unahur.edu.ar>
2022-08-19 19:09:59 -0300

    TDA.py

--- TDA.py
+++ TDA.py
@@ -6,7 +6,8 @@
 • Año de construcción
 • Cantidad de ambientes
 Implementar las siguientes operaciones:
-• Constructor: Debe incluir las validaciones necesarias, teniendo en cuenta que solo
+• Constructor: Debe incluir las validaciones necesarias, 
+teniendo en cuenta que solo
 se almacenan propiedades construidas luego de 1870.
 • _str_: Al usar la función print con una variable del tipo propiedad debe mostrar:
 'calle' 'numero' ('localidad').
@@ -24,7 +25,32 @@ inmobiliario de una propiedad, según la siguiente regla:
 ◦ Propiedades desde 1950 hasta la actualidad:
 ▪ Entre 1 y 5 ambientes: 5% de impuesto
 #▪ Más de 5 ambientes: 35 % de impuesto*/ """
-class Propiedad:
-    def __init__(self,):
-        pass
+import copy as cp
 
+def validarTipo(variable, nombre, tipo):
+    if isinstance(variable, tipo):
+        return variable
+    else:
+        raise Exception("La variable " + nombre + " debe ser de tipo " + str(tipo) + ".")
+def validarAño(año):
+    if (año > 1870):
+        return año
+    else:
+        return "valor invalido"
+class Propiedad:
+  def __init__(self, calle="", numero=0 , localidad="", año=0, ambientes=0):
+    self.__calle=validarTipo(calle,"calle",str)
+    self.__numero = numero
+    self.__localidad = validarTipo(localidad, "localidad", str)
+    self.__año = validarAño(año)
+    self.__ambientes = ambientes
+  def getCalle(self):
+    return self.__calle
+  def modificarCalle(self,calle):
+      self.__calle = calle
+  def __repr__(self):
+    return self.__calle
+     
+
+prop1= Propiedad("repeto",1448,"ituzaingo",1200,4)
+print (prop1)
